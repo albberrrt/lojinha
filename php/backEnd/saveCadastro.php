eaab84cca759aa1cf2850234eb8c3749a06eec32
albberrrt <albberrrt.yayay@gmail.com>
2022-08-19 15:04:04 -0300

    aulaJava

--- php/backEnd/saveCadastro.php
+++ php/backEnd/saveCadastro.php
@@ -8,16 +8,25 @@ if(empty($_POST['formUser']) || empty($_POST['formPass']) || empty($_POST['formE
     header('Location: ../frontEnd/cadastro.php?error=101');
 
 } else {
-    
+
     $formUser = $_POST['formUser'];
     $formPass = $_POST['formPass'];
     $formEmail = $_POST['formEmail'];
-    
+    $profileImg = "../../img/defaultProfile.png";
+    $userCartName = $formUser . "Cart";
+    echo $userCartName;
+
+    $passwordHashed = password_hash($formPass, PASSWORD_DEFAULT);
+
+    $sth = "INSERT INTO `users` (`userId`, `userName`, `userEmail`, `userPassword`, `userCart`, `userImg`) VALUES (null, :userName, :userEmail, :userPassword, :userCart, :userProfileImg)";
+    $insrt = $conn->prepare($sth);
+    $insrt->bindParam(':userName', $formUser, PDO::PARAM_STR);
+    $insrt->bindParam(':userEmail', $formEmail, PDO::PARAM_STR);
+    $insrt->bindParam(':userPassword', $passwordHashed, PDO::PARAM_STR);
+    $insrt->bindParam(':userProfileImg', $profileImg, PDO::PARAM_STR);
+    $insrt->bindParam(':userCart', $userCartName, PDO::PARAM_STR);
+    $insrt->execute();
 
 }
-
-
-
-
 
 ?>
--- sql/database.sql
+++ sql/database.sql
@@ -1,11 +1,12 @@
 CREATE DATABASE BigodeShop;
+USE BigodeShop;
 
 CREATE TABLE users (
     userId INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     userName VARCHAR(255) NOT NULL,
     userEmail VARCHAR(255) NOT NULL,
     userPassword VARCHAR(255) NOT NULL,
-    userCart INT NOT NULL,
+    userCart VARCHAR(255) NOT NULL,
     userImg VARCHAR(255) NOT NULL
     );
 
@@ -13,13 +14,21 @@ CREATE TABLE produtos (
     produtoId INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     produtoPrc INT NOT NULL,
     produtoName VARCHAR(255) NOT NULL,
-    categoriaId VARCHAR(255) NOT NULL,
+    produtoImg VARCHAR(255) NOT NULL,
+    categoriaId INT NOT NULL,
     produtoVendas INT NOT NULL,
     discProduto INT,
-    FOREIGN KEY (`categoriaId`) REFERENCES `categorias` (`catId`)
-   )
+    FOREIGN KEY (`categoriaId`) REFERENCES categorias(`categoriaId`)
+   );
 
 CREATE TABLE categorias (
-    catId INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    categoriaId INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     categoria VARCHAR(255) NOT NULL
+    );
+
+CREATE TABLE profileImages (
+	imageId INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    imagePath VARCHAR(255) NOT NULL
     )
+
+INSERT INTO `categorias` (`categoriaId`, `categoria`) VALUES (NULL, 'Camisa polo'), (NULL, 'Camiseta'), (NULL, 'Regata'), (NULL, 'Casaco'), (NULL, 'Moletom'), (NULL, 'Short')
